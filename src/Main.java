9b533ccc077537fb471ecd93a3d1d81a61cd5e71
satoshi <uglysatoshi@ya.ru>
2023-03-22 23:21:30 +0300

    final!!

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner in = new Scanner(System.in);
-    public static void Base() {
-        System.out.print("Введите амплитуду колебания: "); double a = in.nextDouble();
-        int T = 2; double m = 0.2;
-        double W = (2 * Math.pow(Math.PI, 2) * Math.pow(a, 2) * m) / Math.pow(T, 2);
-        System.out.println("Энергия материальной точки: " + W);
+    public static void Base() { // функция базового уровня
+        // Так как ввод T в функцию происходит через константы, следовательно ограничение ввода не требуется и деления на ноль никогда не произойдет
+        System.out.print("Введите амплитуду колебания: "); double a = in.nextDouble(); // ввод значения a
+        final int T = 2; // объявляем T как константу и передаем в нее 2
+        final double m = 0.2; // объявляем m как константу и передаем в нее 0.2
+        double W = (2 * Math.pow(Math.PI, 2) * Math.pow(a, 2) * m) / Math.pow(T, 2); // функция W
+        System.out.println("Энергия материальной точки: " + W); // вывод W
     }
 
     public static void Medium() {
-        final int m = 2;
-        final double x = 1.1;
-        double y = Math.sin((Math.sqrt(Math.abs(x))) + Math.pow(Math.tan(Math.pow(x, 4) + Math.pow(m, 2)), 3));
-        System.out.println("Функция Y: " + y);
+        // так как под корнем находится модуль, корень не может быть отрицательным
+        final int m = 2; // объявляем m как константу и передаем в нее 2
+        final double x = 1.1; // объявляем x как константу и передаем в нее 1.1
+        double y = Math.sin((Math.sqrt(Math.abs(x))) + Math.pow(Math.tan(Math.toDegrees(Math.pow(x, 4) + Math.pow(m, 2))), 3)); // Функция y
+        System.out.println("Функция Y: " + y); // Выводим y
     }
 
     public static void Hard() {
-        double C = Math.pow(10, -6);
-        double L = 0.04;
-        int U = 100;
-        double I = U * Math.sqrt(C / L);
-        double W = (L * Math.pow(I, 2) / 2);
-        System.out.println("Амплитуда силы тока: " + I + " A");
-        System.out.println("Полная энергия: " + W + " Дж");
+        // Так как ввод значений в функцию происходит через константы, следовательно ограничение ввода не требуется и деления на ноль никогда не произойдет
+        final double C = Math.pow(10, -6); // через функцию pow создаем значение 10^-6  и передаем в C
+        final double L = 0.04; // Передаем в L 0.04
+        final int U = 100; // Передаем в U 100
+        double I = U * Math.sqrt(C / L); // Находим I
+        double W = (L * Math.pow(I, 2) / 2); // Находим W
+        System.out.println("Амплитуда силы тока: " + I + " A"); // Выводим I
+        System.out.println("Полная энергия: " + W + " Дж"); // Выводим W
     }
     public static void main(String[] args) {
         Base();
